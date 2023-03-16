b14a5c83aa2e8c0fdb160022da538d95065edfc6
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2023-03-15 22:11:30 -0300

    Adicionar arquivos de projeto.

---
+++ checkBox e eventos/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace checkBox_e_eventos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maça = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.banana = new System.Windows.Forms.CheckBox();
+            this.manga = new System.Windows.Forms.CheckBox();
+            this.uva = new System.Windows.Forms.CheckBox();
+            this.cereja = new System.Windows.Forms.CheckBox();
+            this.laranja = new System.Windows.Forms.CheckBox();
+            this.goiaba = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maça
+            // 
+            this.maça.AutoSize = true;
+            this.maça.Font = new System.Drawing.Font("Schadow BT", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maça.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.maça.Location = new System.Drawing.Point(52, 104);
+            this.maça.Name = "maça";
+            this.maça.Size = new System.Drawing.Size(58, 19);
+            this.maça.TabIndex = 0;
+            this.maça.Text = "Maça";
+            this.maça.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Pristina", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(59, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escolha a fruta:";
+            // 
+            // banana
+            // 
+            this.banana.AutoSize = true;
+            this.banana.Font = new System.Drawing.Font("Schadow BT", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.banana.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.banana.Location = new System.Drawing.Point(52, 140);
+            this.banana.Name = "banana";
+            this.banana.Size = new System.Drawing.Size(71, 19);
+            this.banana.TabIndex = 2;
+            this.banana.Text = "Banana";
+            this.banana.UseVisualStyleBackColor = true;
+            // 
+            // manga
+            // 
+            this.manga.AutoSize = true;
+            this.manga.Font = new System.Drawing.Font("Schadow BT", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manga.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.manga.Location = new System.Drawing.Point(52, 176);
+            this.manga.Name = "manga";
+            this.manga.Size = new System.Drawing.Size(66, 19);
+            this.manga.TabIndex = 3;
+            this.manga.Text = "Manga";
+            this.manga.UseVisualStyleBackColor = true;
+            // 
+            // uva
+            // 
+            this.uva.AutoSize = true;
+            this.uva.Font = new System.Drawing.Font("Schadow BT", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uva.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.uva.Location = new System.Drawing.Point(52, 214);
+            this.uva.Name = "uva";
+            this.uva.Size = new System.Drawing.Size(49, 19);
+            this.uva.TabIndex = 4;
+            this.uva.Text = "Uva";
+            this.uva.UseVisualStyleBackColor = true;
+            // 
+            // cereja
+            // 
+            this.cereja.AutoSize = true;
+            this.cereja.Font = new System.Drawing.Font("Schadow BT", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cereja.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cereja.Location = new System.Drawing.Point(52, 251);
+            this.cereja.Name = "cereja";
+            this.cereja.Size = new System.Drawing.Size(64, 19);
+            this.cereja.TabIndex = 5;
+            this.cereja.Text = "Cereja";
+            this.cereja.UseVisualStyleBackColor = true;
+            // 
+            // laranja
+            // 
+            this.laranja.AutoSize = true;
+            this.laranja.Font = new System.Drawing.Font("Schadow BT", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.laranja.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.laranja.Location = new System.Drawing.Point(52, 288);
+            this.laranja.Name = "laranja";
+            this.laranja.Size = new System.Drawing.Size(73, 19);
+            this.laranja.TabIndex = 6;
+            this.laranja.Text = "Laranja";
+            this.laranja.UseVisualStyleBackColor = true;
+            // 
+            // goiaba
+            // 
+            this.goiaba.AutoSize = true;
+            this.goiaba.Font = new System.Drawing.Font("Schadow BT", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goiaba.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.goiaba.Location = new System.Drawing.Point(52, 322);
+            this.goiaba.Name = "goiaba";
+            this.goiaba.Size = new System.Drawing.Size(68, 19);
+            this.goiaba.TabIndex = 7;
+            this.goiaba.Text = "Goiaba";
+            this.goiaba.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RosyBrown;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(272, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Escolha a fruta e clique aqui !";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.goiaba);
+            this.Controls.Add(this.laranja);
+            this.Controls.Add(this.cereja);
+            this.Controls.Add(this.uva);
+            this.Controls.Add(this.manga);
+            this.Controls.Add(this.banana);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maça);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox maça;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox banana;
+        private System.Windows.Forms.CheckBox manga;
+        private System.Windows.Forms.CheckBox uva;
+        private System.Windows.Forms.CheckBox cereja;
+        private System.Windows.Forms.CheckBox laranja;
+        private System.Windows.Forms.CheckBox goiaba;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ checkBox e eventos/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkBox_e_eventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txt = "";
+
+            if (maça.Checked)
+            {
+                txt += maça.Text + ",";
+            }
+            if (banana.Checked)
+            {
+                txt += banana.Text + ",";   
+            }
+            if (manga.Checked)
+            {
+                txt+= manga.Text + ","; 
+            }
+            if (uva.Checked)
+            {
+                txt+= uva.Text + ",";
+            }
+            if (cereja.Checked) { 
+            txt += cereja.Text + ",";
+            }
+            if (laranja.Checked)
+            {
+                txt +=(laranja.Text+",");   
+            }
+            if (goiaba.Checked) {
+            txt += goiaba.Text + ",";
+            }
+            MessageBox.Show("A fruta escolhida foi: " + txt, "A fruta escolhida foi ...");
+        }
+    }
+}
